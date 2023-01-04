adc80d08138d48192c30c661f8a929c25317741d
ChongLeangUENG <leangcnrp777@gmail.com>
2023-01-04 18:08:25 +0100

    Fixing the Production Deployment

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,15 @@
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-apt-get update && apt-get install -y hugo make
+type_cpu=$(lscpu|grep Architecture|awk '{print $2}')
+if [ $type_cpu == "aarch64" ]
+then
+    type_cpu="arm64"
+elif [ $type_cpu == "x86_64" ]
+then
+    type_cpu="64bit"
+fi
+if [ ! -f "./hugo_0.109.0_Linux-"$type_cpu".deb" ]
+then
+    curl -L "https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_0.109.0_linux-"$type_cpu".deb" -o last.deb
+fi
+apt install ./last.deb
+rm last.deb
 make build
