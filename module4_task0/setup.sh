2a799f68cc92af770da8469557f6e3f3d77a16d6
ChongLeangUENG <leangcnrp777@gmail.com>
2023-01-12 10:57:15 +0100

    Updare

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get install go
-npm install -g markdownlint-cli
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+sudo apt-get install ./install_hugo.deb
